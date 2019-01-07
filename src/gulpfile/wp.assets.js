f9e8223d81a28dda4d1b6f5bcc0c99f47fe15a97
Kouyou Nomiyama <kouyou0277@gmail.com>
2019-01-07 15:25:16 +0900

    ディレクトリ直下にwp_assets/customize.scssを編集スタイル用cssで書き出し化

--- src/gulpfile/wp.assets.js
+++ src/gulpfile/wp.assets.js
@@ -9,17 +9,22 @@ var $           = require('gulp-load-plugins')({
   replaceString: /\bgulp[\-.]/
 });
 
-if(config.mode.cms){
-  gulp.task('wp.assets',function () {
-    // wordpress用 style.css 書き出し
-    if(config.mode.cmstype === "wordpress") {
-      gulp.src(config.path.source + config.path.wp_assets + '**/*.scss')
-        .pipe($.sass())
-        .pipe($.rename('style.css'))
-        .pipe(gulp.dest(config.path.cms + config.path.cms_dir + config.path.cms_theme_path + config.path.cms_theme_name))
+if (config.mode.cms) {
+    gulp.task('wp.assets', function () {
+        // wordpress用 style.css 書き出し
+        if (config.mode.cmstype === "wordpress") {
+            gulp.src(config.path.source + config.path.wp_assets + 'wp_style.scss')
+                .pipe($.sass())
+                .pipe($.rename('style.css'))
+                .pipe(gulp.dest(config.path.cms + config.path.cms_dir + config.path.cms_theme_path + config.path.cms_theme_name))
 
-      gulp.src(config.path.source + config.path.wp_assets + '**/*.png')
-        .pipe(gulp.dest(config.path.cms + config.path.cms_dir + config.path.cms_theme_path + config.path.cms_theme_name))
-    }
-  });
+            gulp.src(config.path.source + config.path.wp_assets + '**/*.scss' || config.path.source + config.path.wp_assets + '!wp_style.scss')
+                .pipe($.sass())
+                .pipe(gulp.dest(config.path.cms + config.path.cms_dir + config.path.cms_theme_path + config.path.cms_theme_name))
+
+            gulp.src(config.path.source + config.path.wp_assets + '**/*.png')
+                .pipe(gulp.dest(config.path.cms + config.path.cms_dir + config.path.cms_theme_path + config.path.cms_theme_name))
+
+        }
+    });
 }
