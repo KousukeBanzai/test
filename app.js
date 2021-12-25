34404c2d1cce920bb7111575dc09e1b558694835
KousukeBanzai <kousuke.banzai@gmail.com>
2021-12-26 06:40:37 +0900

    feat : scssコンパイル、Vue.jsを追加

--- app.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!********************!*\
-  !*** ./src/app.js ***!
-  \********************/
-// src/app.js
-alert('hello world');
-/******/ })()
-;
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,5 +4,26 @@ const srcDir = './';
 const assetsDir = 'assets';
 const distDir = '../';
 
-mix.js(srcDir + assetsDir + '/js/app.js', './' + assetsDir + '/js/')
+mix.js(srcDir + assetsDir + '/js/app.js',
+  './' + assetsDir + '/js/').vue()
    .setPublicPath(distDir);
+
+mix.sass(srcDir + assetsDir + '/scss/app.scss',
+  './' + assetsDir + '/css/',
+  {
+    // Prefer `dart-sass`
+    implementation: require("sass"),
+    sourceMap: true,
+    // sassOptions: {
+    //   outputStyle: "compressed",
+    // },
+  });
+
+mix.browserSync({
+  files: ["./*", "./**/*", "./**/**/*", "./**/**/**/*"],
+  //server: '../../../../',
+  browser: "google chrome",
+  proxy: {
+    target: "http://localhost/",
+  },
+});
