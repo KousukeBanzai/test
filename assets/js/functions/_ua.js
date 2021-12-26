1034cd5fdae0a050399b1f823a226063736d4394
KousukeBanzai <kousuke.banzai@gmail.com>
2021-12-26 10:06:37 +0900

    feat : 外部jsファイルをインクルード

---
+++ assets/js/functions/_ua.js
@@ -0,0 +1,6 @@
+export function hoge(param) {
+  
+  param = param * 2;
+  
+  return param;
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
+import * as func from './functions/_ua';
 import Vue from 'vue';
 
 new Vue({
   el: '#app',
 });
 
+const param = 5;
+const result = func.hoge(param);
+
+console.log(result);
